2325880811a92d723a974132f879a0d484244b7f
sunnyandgood <wrhsunny@163.com>
2019-01-07 13:25:35 +0800

    项目创建

---
+++ src/main/java/com/edu/util/Test.java
@@ -0,0 +1,11 @@
+package com.edu.util;
+
+/**
+ * @Author: 王仁洪
+ * @Date: 2019/1/7 13:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("rdytf");
+    }
+}
